7ab55677cc0d57d6783bbb367676a24630c6ca10
Nyarla4 <31647681+Nyarla4@users.noreply.github.com>
2025-11-21 19:48:21 +0900

    야바위 작업 수정
    속도 조정 및 플로우 수정 필요
    Reset이랑 별개로 그대로 다시 닫고 섞는거 필요할듯

--- Assets/Scripts/Tasks/YabawiTask.cs
+++ Assets/Scripts/Tasks/YabawiTask.cs
@@ -5,201 +5,194 @@ using UnityEngine.UI;
 
 public class YabawiTask : TaskBase
 {
-	public Button[] Cups;
-	[SerializeField] private int _shuffleCount = 3;
-	public override string Name => "Yabawi";
+    public Button[] Cups;
+    [SerializeField] private int _shuffleCount = 3;
+    private float _openCloseSpeed = 20f;
+    private float _moveSpeed = 5f;
+    public override string Name => "Yabawi";
 
-	public override void ResetTask()
-	{
-		StopAllCoroutines();
-		Cups[0].transform.localPosition = new Vector3(-115f, 0f, 0f);
-		Cups[1].transform.localPosition = new Vector3(0f, 0f, 0f);
-		Cups[2].transform.localPosition = new Vector3(115f, 0f, 0f);
-		Cups[0].transform.GetChild(0).localPosition = Vector3.zero;
-		Cups[1].transform.GetChild(0).localPosition = Vector3.zero;
-		Cups[2].transform.GetChild(0).localPosition = Vector3.zero;
-		//foreach (Image img in patternStageLights)
-		//	img.color = stageLightOff;
-		//foreach (Image img in patternSquares)
-		//	img.color = patternSquareOff;
-		//foreach (Image img in matchStageLights)
-		//	img.color = stageLightOff;
-		//foreach (Button btn in matchButtons)
-		//	btn.interactable = false;
-		//pattern.Clear();
-		//match.Clear();
+    private Coroutine _shuffleRoutine = null;
+    public override void ResetTask()
+    {
+        StopAllCoroutines();
+        _shuffleRoutine = null;
 
-		if (gameObject.activeInHierarchy)
-			StartCoroutine(Shuffle());
-	}
-
-	private void OnEnable()
-	{
-		StartCoroutine(Shuffle());
-	}
-
-	/// <summary>
-	/// Shuffle
-	///		
-	/// </summary>
-	IEnumerator Shuffle()
-	{
-		float imgY = 0f;
-		
-		//컵 올리고
-        while (imgY < 100f)
+        Cups[0].transform.localPosition = new Vector3(-115f, 0f, 0f);
+        Cups[1].transform.localPosition = new Vector3(0f, 0f, 0f);
+        Cups[2].transform.localPosition = new Vector3(115f, 0f, 0f);
+        Cups[0].transform.GetChild(0).localPosition = Vector3.zero;
+        Cups[1].transform.GetChild(0).localPosition = Vector3.zero;
+        Cups[2].transform.GetChild(0).localPosition = Vector3.zero;
+        
+        if (gameObject.activeInHierarchy)
         {
-			imgY += Time.deltaTime;
-            for (int i = 0; i < Cups.Length; i++)
-            {
-				var cup = Cups[i];
-				var pos = cup.transform.GetChild(0).localPosition;
-				pos.y = imgY;
-				cup.transform.GetChild(0).localPosition = pos;
-			}
-		}
-		imgY = 100f;
-		for (int i = 0; i < Cups.Length; i++)
-		{
-			var cup = Cups[i];
-			var pos = cup.transform.GetChild(0).localPosition;
-			pos.y = imgY;
-			cup.transform.GetChild(0).localPosition = pos;
-		}
+            _shuffleRoutine = StartCoroutine(Shuffle());
+        }
+    }
 
-		//0.25초대기
-		yield return new WaitForSeconds(0.25f);
+    private void OnEnable()
+    {
+        _shuffleRoutine = StartCoroutine(Shuffle());
+    }
 
-		//컵 내리고
-		while (imgY > 0)
-		{
-			imgY -= Time.deltaTime;
-			for (int i = 0; i < Cups.Length; i++)
-			{
-				var cup = Cups[i];
-				var pos = cup.transform.GetChild(0).localPosition;
-				pos.y = imgY;
-				cup.transform.GetChild(0).localPosition = pos;
-			}
-		}
-		imgY = 0f;
-		for (int i = 0; i < Cups.Length; i++)
-		{
-			var cup = Cups[i];
-			var pos = cup.transform.GetChild(0).localPosition;
-			pos.y = imgY;
-			cup.transform.GetChild(0).localPosition = pos;
-		}
+    /// <summary>
+    /// Shuffle
+    /// </summary>
+    IEnumerator Shuffle()
+    {
+        float deltaSpeed = Time.deltaTime * _moveSpeed;
 
-		//_shuffleCount 회 셔플
-		for (int i = 0; i < _shuffleCount; i++)
+        yield return StartCoroutine(OpenCups());
+
+        //0.25초대기
+        yield return new WaitForSeconds(0.25f);
+
+        yield return StartCoroutine(CloseCups());
+
+        //_shuffleCount 회 셔플
+        for (int i = 0; i < _shuffleCount; i++)
         {
-			//섞을 대상
-			int notTarget = Random.Range(0, 3);
-			int target1Idx = -1;
-			int target2Idx = -1;
+            //섞을 대상
+            int notTarget = Random.Range(0, 3);
+            int target1Idx = -1;
+            int target2Idx = -1;
             switch (notTarget)
             {
-				case 0:
-					target1Idx = 1;
-					target2Idx = 2;
+                case 0:
+                    target1Idx = 1;
+                    target2Idx = 2;
                     break;
-				case 1:
-					target1Idx = 0;
-					target2Idx = 2;
+                case 1:
+                    target1Idx = 0;
+                    target2Idx = 2;
                     break;
-				case 2:
-					target1Idx = 0;
-					target2Idx = 1;
+                case 2:
+                    target1Idx = 0;
+                    target2Idx = 1;
                     break;
             }
-			Button target1 = Cups[target1Idx];
-			float target1X = target1.transform.localPosition.x;
-			Button target2 = Cups[target2Idx];
-			float target2X = target2.transform.localPosition.x;
+            Button target1 = Cups[target1Idx];
+            float target1X = target1.transform.localPosition.x;
+            Button target2 = Cups[target2Idx];
+            float target2X = target2.transform.localPosition.x;
 
-			float timer = 0;
+            float timer = 0;
             while (timer < 1f)
             {
-				timer += Time.deltaTime;
-				Vector3 tar1Pos = target1.transform.localPosition;
-				tar1Pos.x = Mathf.Lerp(target1X, target2X, timer / 1f);
-				target1.transform.localPosition = tar1Pos;
-				Vector3 tar2Pos = target2.transform.localPosition;
-				tar2Pos.x = Mathf.Lerp(target2X, target1X, timer / 1f);
-				target2.transform.localPosition = tar2Pos;
-			}
+                timer += deltaSpeed;
+                Vector3 tar1Pos = target1.transform.localPosition;
+                tar1Pos.x = Mathf.Lerp(target1X, target2X, timer / 1f);
+                target1.transform.localPosition = tar1Pos;
+                Vector3 tar2Pos = target2.transform.localPosition;
+                tar2Pos.x = Mathf.Lerp(target2X, target1X, timer / 1f);
+                target2.transform.localPosition = tar2Pos;
+                yield return null;
+            }
         }
-		//match.Clear();
-		//
-		//foreach (Image img in matchStageLights)
-		//	img.color = stageLightOff;
-		//
-		//foreach (Button btn in matchButtons)
-		//	btn.interactable = false;
-		//
-		//yield return new WaitForSeconds(0.75f);
-		//
-		//pattern.Add((byte)Random.Range(0, 9));
-		//patternStageLights[pattern.Count - 1].color = stageLightOn;
-		//
-		//foreach (byte b in pattern)
-		//{
-		//	patternSquares[b].color = patternSquareOn;
-		//	AudioManager.Play("simonUI", AudioManager.MixerTarget.UI, null, pitches[b]);
-		//	yield return new WaitForSeconds(0.4f);
-		//	patternSquares[b].color = patternSquareOff;
-		//	yield return new WaitForSeconds(0.1f);
-		//}
-		//
-		//foreach (Button btn in matchButtons)
-		//	btn.interactable = true;
-	}
 
-	public void PressMatch(int index)
-	{
-		if(index == 0)
+        _shuffleRoutine = null;
+    }
+
+    private IEnumerator OpenCups()
+    {
+        float imgY = 0f;
+        float deltaSpeed = Time.deltaTime * _openCloseSpeed;
+
+        //컵 올리고
+        while (imgY < 100f)
         {
-			Completed();
+            imgY += deltaSpeed;
+            for (int i = 0; i < Cups.Length; i++)
+            {
+                var cup = Cups[i];
+                var pos = cup.transform.GetChild(0).localPosition;
+                pos.y = imgY;
+                cup.transform.GetChild(0).localPosition = pos;
+            }
+            yield return null;
+        }
+        imgY = 100f;
+        for (int i = 0; i < Cups.Length; i++)
+        {
+            var cup = Cups[i];
+            var pos = cup.transform.GetChild(0).localPosition;
+            pos.y = imgY;
+            cup.transform.GetChild(0).localPosition = pos;
+        }
+    }
+    
+    private IEnumerator CloseCups()
+    {
+        float imgY = 0f;
+        float deltaSpeed = Time.deltaTime * _openCloseSpeed;
+
+        //컵 내리고
+        while (imgY > 0)
+        {
+            imgY -= deltaSpeed;
+            for (int i = 0; i < Cups.Length; i++)
+            {
+                var cup = Cups[i];
+                var pos = cup.transform.GetChild(0).localPosition;
+                pos.y = imgY;
+                cup.transform.GetChild(0).localPosition = pos;
+            }
+            yield return null;
+        }
+        imgY = 0f;
+        for (int i = 0; i < Cups.Length; i++)
+        {
+            var cup = Cups[i];
+            var pos = cup.transform.GetChild(0).localPosition;
+            pos.y = imgY;
+            cup.transform.GetChild(0).localPosition = pos;
+        }
+    }
+
+    private IEnumerator CheckCups(int index)
+    {
+        float imgY = 0f;
+        float deltaSpeed = Time.deltaTime * _openCloseSpeed;
+
+        //컵 올리고
+        while (imgY < 100f)
+        {
+            imgY += deltaSpeed;
+            for (int i = 0; i < Cups.Length; i++)
+            {
+                var cup = Cups[i];
+                var pos = cup.transform.GetChild(0).localPosition;
+                pos.y = imgY;
+                cup.transform.GetChild(0).localPosition = pos;
+            }
+            yield return null;
+        }
+        imgY = 100f;
+        for (int i = 0; i < Cups.Length; i++)
+        {
+            var cup = Cups[i];
+            var pos = cup.transform.GetChild(0).localPosition;
+            pos.y = imgY;
+            cup.transform.GetChild(0).localPosition = pos;
+        }
+
+        if (index == 0)
+        {
+            Completed();
         }
         else
         {
-			ResetTask();
-		}
+            ResetTask();
+        }
+    }
 
-		//match.Add(index);
-		//AudioManager.Play("simonUI", AudioManager.MixerTarget.UI, null, pitches[index]);
-		//matchStageLights[match.Count - 1].color = stageLightOn;
-		//
-		//if (index != pattern[match.Count - 1])
-		//	StartCoroutine(WrongInput());
-		//else if (match.Count == pattern.Count)
-		//{
-		//	if (pattern.Count == 5)
-		//		StartCoroutine(DelayCompleted());
-		//	else
-		//		StartCoroutine(ShowPattern());
-		//}
-	}
+    public void PressMatch(int index)
+    {
+        if (_shuffleRoutine != null)
+        {
+            return;
+        }
 
-	IEnumerator WrongInput()
-	{
-		//foreach (Button btn in matchButtons)
-		//	btn.interactable = false;
-		//
-		//foreach (Image img in patternSquares)
-		//	img.color = patternSquareWrong;
-
-		yield return new WaitForSeconds(1);
-
-		ResetTask();
-	}
-
-	IEnumerator DelayCompleted()
-	{
-		//foreach (Image img in patternSquares)
-		//	img.color = patternSquareOn;
-		yield return new WaitForSeconds(0.5f);
-		Completed();
-	}
+        StartCoroutine(CheckCups(index));
+    }
 }
